84f441e65937eb216b6de464a0fb8419d7cf4293
heckfyxe <heckfyxe@gmail.com>
2020-11-17 23:09:56 +0300

    Added card_holder.rb. Class contains shuffled cards and pops them one by one.

---
+++ card_holder.rb
@@ -0,0 +1,22 @@
+require_relative 'card'
+
+class CardHolder
+  RANKS = Array.new(13) { |i| i + 2 <= 10 ? (i + 2).to_s : %w[В Д К T][i - 10] }
+  SUITS = %w[♦ ♣ ♥ ♠]
+
+  def initialize
+    @cards = []
+    shuffle_cards
+  end
+
+  def shuffle_cards
+    RANKS.each do |rank|
+      SUITS.each { |suit| @cards << Card.new(rank, suit) }
+    end
+    @cards.shuffle!
+  end
+
+  def pop
+    @cards.pop
+  end
+end
